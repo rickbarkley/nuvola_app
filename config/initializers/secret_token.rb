2e09060942fe3d0253b5fcb30156f34ab5ab145e
Rick Barkley <rick_barkley@nuvola-networks.com>
2013-06-12 15:02:17 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuvolaApp::Application.config.secret_token = '5ed60366ad3fcd06a5983df8f4701d6e492fe867e15ba921d2158d7ad6853ec1a100c34bbfd1777838ee28ff026d8d8cc7fd14744ff5f598623f17d1156029fc'
